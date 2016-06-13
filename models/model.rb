a662e61df645d9777ba362802ba6d4a700fa0a27
zenzile <tonge1@ms324.org>
2016-06-13 14:18:24 +0000

    hello

---
+++ models/model.rb
@@ -0,0 +1,13 @@
+countries = {
+:China=> {"QQ candy", "dried plums", "prepared shredded squid","white rabbit milk candy", "lychee jell-o", "Hong Kong egg waffles", "roasted chestnuts","binganghulu", "chinese sugar painting candy"}
+:Dominican_Republic=>{"platanos", "morisonado", "pastelitos de guayaba", "bizcocho dominicano","Habichueplas Con Dulce"}
+:Barbados=>{"flying fish"}
+:Haiti=>{"blanc manger", "Tablet cocoye", "bonbon sirop", "diz riz au leit", "gateau de manioc"}
+:Jamica=>{"gizzada", "coconut drop","grater cake"}
+:Brazil=>{"fandagos","bala chita", "melzinho"}
+:Puerto_Rico=>{"bacalaitos","mofongo","flan"}
+:Hong_Kong=>{"pineapple bun", "steam veg bun(Choi bao)", "lo mai gai"}
+:Phillippines=>{"adobo", "pastillas de leche", "turrones de Kasoy"}
+:Trinidad=>{"sugar cake"}
+    
+}                                            
